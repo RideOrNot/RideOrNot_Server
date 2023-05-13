9e97696c8d8aa9993aec5976fc7b88e91046b71c
hamseungho <bc1206@naver.com>
2023-05-13 20:50:40 +0900

    initialcommit

---
+++ src/main/java/com/example/hanium2023/Hanium2023Application.java
@@ -0,0 +1,13 @@
+package com.example.hanium2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanium2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hanium2023Application.class, args);
+    }
+
+}
