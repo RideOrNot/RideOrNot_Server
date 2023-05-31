f4f897f76b8b04f9303556a3432d10e501883c8b
qjvk2880 <qjvk2880@gmail.com>
2023-05-31 17:14:13 +0900

    Chore : 의존성 수정

--- src/test/java/com/example/hanium2023/Hanium2023ApplicationTests.java
+++ src/test/java/com/example/hanium2023/Hanium2023ApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.hanium2023;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Hanium2023ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class Hanium2023ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
