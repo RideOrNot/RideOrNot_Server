120165d6db57c55452e3e532b26cc45795ac8ecd
qjvk2880 <qjvk2880@gmail.com>
2023-07-02 15:38:55 +0900

    Feat : 사용자 엔티티 추가

---
+++ src/main/java/com/example/hanium2023/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.hanium2023.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String nickname;
+    private double speed;
+    private int ageRange;
+    private int gender;
+    private String password;
+    private double speedWeight;
+
+}
