a0217c24856dac37b5f1f191bc17e247c2949a4d
sjhlko <sjhlko@naver.com>
2023-07-13 11:09:49 +0900

    Feat : Swagger 문서에 설명 추가

--- src/main/java/com/example/hanium2023/controller/DataInsertController.java
+++ src/main/java/com/example/hanium2023/controller/DataInsertController.java
@@ -3,6 +3,7 @@ package com.example.hanium2023.controller;
 import com.example.hanium2023.domain.dto.ressponse.Response;
 import com.example.hanium2023.service.StationExitService;
 import com.example.hanium2023.service.StationService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ import java.io.IOException;
 public class DataInsertController {
     private final StationExitService stationExitService;
     private final StationService stationService;
+    @ApiOperation(value = "초기 데이터 삽입을 위한 api 이므로 호출하지 말것.(현재는 접근 제한됨)")
     @GetMapping("/add-exit")
     public Response<String> addExit() throws IOException, InterruptedException {
         //접근 제한을 위한 주석처리
         //stationExitService.addExit();
         return Response.success("add");
     }
+    @ApiOperation(value = "초기 데이터 삽입을 위한 api 이므로 호출하지 말것.(현재는 접근 제한됨)")
     @GetMapping("/add-station")
     public Response<String> addStation() throws IOException, InterruptedException {
         //접근 제한을 위한 주석처리
