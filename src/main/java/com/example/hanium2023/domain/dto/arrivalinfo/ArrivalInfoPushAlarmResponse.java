adc5236a2b4ddea244961bb334a46b6c7f310d73
qjvk2880 <qjvk2880@gmail.com>
2023-07-15 21:47:37 +0900

    Refactor : 푸시 알림을 위한 도착 정보를 새로운 DTO에 담게끔 수정

--- src/main/java/com/example/hanium2023/domain/dto/arrivalinfo/ArrivalInfoPushAlarmResponse.java
+++ src/main/java/com/example/hanium2023/domain/dto/arrivalinfo/ArrivalInfoPushAlarmResponse.java
@@ -1,29 +1,27 @@
 package com.example.hanium2023.domain.dto.arrivalinfo;
 
 
-import com.example.hanium2023.domain.dto.arrivalinfo.ArrivalInfoApiResult;
 import lombok.Data;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
-public class ArrivalInfoResponse {
+public class ArrivalInfoPushAlarmResponse {
     long arrivalTime;
     long movingTime;
     String direction;
-    String lineName;
+    String lineId;
     String destination;
     String message;
     int movingSpeedStep;
     String currentTime;
     String stationName;
 
-    public ArrivalInfoResponse(ArrivalInfoApiResult apiResult,String stationName) {
+    public ArrivalInfoPushAlarmResponse(ArrivalInfoApiResult apiResult, String stationName) {
         this.arrivalTime = apiResult.getArrivalTime();
         this.direction = apiResult.getDirection();
-        this.lineName = apiResult.getLineName();
+        this.lineId = apiResult.getLineId();
         this.destination = apiResult.getDestination();
         this.currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm:ss"));
         this.stationName = stationName;
--- src/main/java/com/example/hanium2023/domain/dto/arrivalinfo/PushAlarmResponse.java
+++ src/main/java/com/example/hanium2023/domain/dto/arrivalinfo/PushAlarmResponse.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class PushAlarmResponse {
-    List<ArrivalInfoResponse> arrivalInfo;
+    List<ArrivalInfoPushAlarmResponse> arrivalInfo;
 }
