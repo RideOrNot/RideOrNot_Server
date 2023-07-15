3bf86a5e88c3483e465342f69d849bcd21c5a63c
qjvk2880 <qjvk2880@gmail.com>
2023-07-15 21:48:16 +0900

    Feat : 역, 호선별 혼잡도와 도착정보를 응답하는 API 추가

--- src/main/java/com/example/hanium2023/controller/StationController.java
+++ src/main/java/com/example/hanium2023/controller/StationController.java
@@ -1,22 +1,37 @@
 package com.example.hanium2023.controller;
 
+import com.example.hanium2023.domain.dto.arrivalinfo.PushAlarmResponse;
+import com.example.hanium2023.domain.dto.ressponse.Response;
+import com.example.hanium2023.domain.dto.station.StationInfoPageResponse;
+import com.example.hanium2023.service.PublicApiService;
 import com.example.hanium2023.service.StationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/station")
+@RequestMapping("/stations")
 public class StationController {
     private final StationService stationService;
+    private final PublicApiService publicApiService;
+
+    @GetMapping("/arrivalInfo")
+    public Response<PushAlarmResponse> getRealTimeInfoForPushAlarm(@RequestParam String stationName, @RequestParam String exitName) {
+        return Response.success(publicApiService.getRealTimeInfoForPushAlarm(stationName, exitName));
+    }
+
+    @GetMapping("/{stationName}/{lineId}")
+    public Response<StationInfoPageResponse> getStationInfo(@PathVariable("stationName") String stationName, @PathVariable("lineId") String lineId) {
+        return Response.success(stationService.getStationInfo(stationName,lineId));
+    }
+
     @GetMapping("/insert/distance")
-    public String insertDistances(){
+    public String insertDistances() {
         return stationService.insertDistances();
     }
+
     @GetMapping("/insert/stationId")
-    public String insertStationId(){
+    public String insertStationId() {
         return stationService.insertStationId();
     }
 }
