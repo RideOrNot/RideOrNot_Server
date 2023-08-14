c3e7ebf920b372a02d0f1c0b0c91d9472dcc1964
sjhlko <sjhlko@naver.com>
2023-08-14 10:31:27 +0900

    Fix : @Repository 추가

--- src/main/java/com/example/hanium2023/repository/LineRepository.java
+++ src/main/java/com/example/hanium2023/repository/LineRepository.java
@@ -2,9 +2,12 @@ package com.example.hanium2023.repository;
 
 import com.example.hanium2023.domain.entity.Line;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LineRepository extends JpaRepository<Line, Integer> {
     Optional<Line> findByCsvLine(Integer csvLine);
+    Optional<Line> findByLineNameContains(String line);
 }
