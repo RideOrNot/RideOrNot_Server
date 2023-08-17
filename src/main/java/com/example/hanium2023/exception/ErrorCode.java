03c5876d60351562edfe2e3d7b81d38bb298bb47
sjhlko <sjhlko@naver.com>
2023-08-17 19:05:38 +0900

    Feat : 공공 api 호출 관련 error code 추가

--- src/main/java/com/example/hanium2023/exception/ErrorCode.java
+++ src/main/java/com/example/hanium2023/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러")
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+    PUBLIC_API_ERROR(HttpStatus.BAD_REQUEST, "Public api 호출 에러")
     ;
     private HttpStatus status;
     private String message;
