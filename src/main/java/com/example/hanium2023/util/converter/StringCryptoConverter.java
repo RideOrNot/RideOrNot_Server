e666840568013a1fcebb303719f0a53c93136ef2
qjvk2880 <qjvk2880@gmail.com>
2023-08-25 02:25:45 +0900

    Feat : 닉네임 암호화 기능 추가

--- src/main/java/com/example/hanium2023/util/converter/StringCryptoConverter.java
+++ src/main/java/com/example/hanium2023/util/converter/StringCryptoConverter.java
@@ -3,31 +3,46 @@ package com.example.hanium2023.util.converter;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Convert;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 @Convert
 public class StringCryptoConverter implements AttributeConverter<String, String> {
-    @Value("${spring.database.column.encrypt.key}")
-    private String encryptKey;
-    private Cipher encryptCipher;
-    private Cipher decryptCipher;
+    @Value("${spring.database.encrypt.key}")
+    private String dbEncryptKey;
 
-    @PostConstruct
-    public void init() throws Exception{
-        encryptCipher = Cipher.getInstance("AES");
-        encryptCipher.init(Cipher.ENCRYPT_MODE, generateMySQLAESKey(key, "UTF-8"));
-        decryptCipher = Cipher.getInstance("AES");
-        decryptCipher.init(Cipher.DECRYPT_MODE, generateMySQLAESKey(key, "UTF-8"));
-    }
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        return null;
+        try {
+            Cipher encryptCipher;
+            byte[] dbEncryptKeyBytes = dbEncryptKey.getBytes(StandardCharsets.UTF_8);
+            encryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(dbEncryptKeyBytes, "AES"));
+            return new String(Base64.getEncoder().encode(encryptCipher.doFinal(attribute.getBytes())));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        return null;
+        try {
+            Cipher decryptCipher;
+            byte[] dbEncryptKeyBytes = dbEncryptKey.getBytes(StandardCharsets.UTF_8);
+            decryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            decryptCipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(dbEncryptKeyBytes, "AES"));
+            return new String(decryptCipher.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
