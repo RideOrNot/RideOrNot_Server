8e0269b5ce68db83e5d49f367a5b7c9e256e33ec
qjvk2880 <qjvk2880@gmail.com>
2023-08-29 19:09:32 +0900

    Refactor : ArrivalCodeEnum에 status 필드 추가

--- src/main/java/com/example/hanium2023/enums/ArrivalCodeEnum.java
+++ src/main/java/com/example/hanium2023/enums/ArrivalCodeEnum.java
@@ -6,12 +6,22 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ArrivalCodeEnum {
-    ENTER(0),
-    ARRIVE(1),
-    DEPART(2),
-    DEPART_BEFORE_STATION(3),
-    ENTER_BEFORE_STATION(4),
-    ARRIVE_BEFORE_STATION(5),
-    NOT_CLOSE_STATION(99);
+    ENTER(0, "진입"),
+    ARRIVE(1, "도착"),
+    DEPART(2, "출발"),
+    DEPART_BEFORE_STATION(3, "전역 출발"),
+    ENTER_BEFORE_STATION(4, "전역 진입"),
+    ARRIVE_BEFORE_STATION(5, "전역 도착"),
+    NOT_CLOSE_STATION(99, "멀리 있음");
     private final int code;
+    private final String status;
+
+    public static ArrivalCodeEnum getEnumByCode(int code) {
+        for (ArrivalCodeEnum value : ArrivalCodeEnum.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
