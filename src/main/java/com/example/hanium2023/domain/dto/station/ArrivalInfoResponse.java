243a6e5608b3bdb7db9e4a1239667080fc95e00d
qjvk2880 <qjvk2880@gmail.com>
2023-09-03 16:44:04 +0900

    Refactor : 타임스탬프 생성 방식 변경

--- src/main/java/com/example/hanium2023/domain/dto/station/ArrivalInfoResponse.java
+++ src/main/java/com/example/hanium2023/domain/dto/station/ArrivalInfoResponse.java
@@ -1,6 +1,8 @@
 package com.example.hanium2023.domain.dto.station;
 
 import com.example.hanium2023.domain.dto.publicapi.arrivalinfo.ArrivalInfoStationInfoPageResponse;
+import com.example.hanium2023.util.TimeUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,10 +13,13 @@ import java.util.Locale;
 @Data
 public class ArrivalInfoResponse {
     List<ArrivalInfoStationInfoPageResponse> arrivalInfo;
-    String currentTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd a HH:mm:ss", timezone = "Asia/Seoul", locale = "ko")
+    LocalDateTime currentTime;
 
     public ArrivalInfoResponse(List<ArrivalInfoStationInfoPageResponse> arrivalInfo) {
         this.arrivalInfo = arrivalInfo;
-        this.currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
+//        this.currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
+        this.currentTime = TimeUtil.getCurrentTime();
+
     }
 }
--- src/main/java/com/example/hanium2023/domain/dto/station/StationInfoPageResponse.java
+++ src/main/java/com/example/hanium2023/domain/dto/station/StationInfoPageResponse.java
@@ -1,6 +1,8 @@
 package com.example.hanium2023.domain.dto.station;
 
 import com.example.hanium2023.domain.dto.publicapi.arrivalinfo.ArrivalInfoStationInfoPageResponse;
+import com.example.hanium2023.util.TimeUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,10 +14,12 @@ import java.util.Locale;
 public class StationInfoPageResponse {
     List<ArrivalInfoStationInfoPageResponse> arrivalInfo;
     String congestion;
-    String currentTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd a HH:mm:ss", timezone = "Asia/Seoul", locale = "ko")
+    LocalDateTime currentTime;
     public StationInfoPageResponse(List<ArrivalInfoStationInfoPageResponse> arrivalInfo, String congestion) {
         this.arrivalInfo = arrivalInfo;
         this.congestion = congestion;
-        this.currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
+//        this.currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
+        this.currentTime = TimeUtil.getCurrentTime();
     }
 }
