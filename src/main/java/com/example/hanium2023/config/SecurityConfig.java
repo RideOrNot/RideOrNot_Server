51f876be1446a3d93cc6898087b57a439536b904
Seungho <bc1206@naver.com>
2023-09-07 11:30:10 +0900

    Refactor : jwt 필터 기능 추가

--- src/main/java/com/example/hanium2023/config/SecurityConfig.java
+++ src/main/java/com/example/hanium2023/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.hanium2023.config;
 
+import com.example.hanium2023.service.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,15 +18,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    private final String secretKey = "0123456789012345678901234567890123456789012"; // JWT 서명을 위한 비밀 키
     // 암호화에 필요한 PasswordEncoder 를 Bean 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,7 +55,22 @@ public class SecurityConfig {
     }
 
     //위 방식 문제점 있음 -> 이제 WebSecurityConfigurerAdapter 지원안함
-    @Bean
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.addFilterBefore(new JwtAuthenticationFilter(secretKey), BasicAuthenticationFilter.class);
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/comments").hasRole("USER")
+                .antMatchers("/**").permitAll();
+    }
+}
+
+
+
+/*@Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
@@ -59,6 +79,4 @@ public class SecurityConfig {
                 .antMatchers("/comments").hasRole("USER")
                 .antMatchers("/**").permitAll(); // 그외 나머지 요청은 누구나 접근 가능
         return http.build();
-    }
-}
-
+    }*/
