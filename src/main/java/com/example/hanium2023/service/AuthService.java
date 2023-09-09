e56112fa5f230df2eb1a36f02e1c1bfe2cd8b9f6
Seungho <bc1206@naver.com>
2023-09-09 16:33:04 +0900

    Refactor : 프로필 업데이트 기능 추가(닉네임, 성별, 연령대)

--- src/main/java/com/example/hanium2023/service/AuthService.java
+++ src/main/java/com/example/hanium2023/service/AuthService.java
@@ -1,11 +1,15 @@
 package com.example.hanium2023.service;
 
+import com.example.hanium2023.domain.dto.user.UserDto;
 import com.example.hanium2023.domain.entity.User;
 import com.example.hanium2023.repository.UserRepository;
 import com.example.hanium2023.util.JwtTokenProvider;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
@@ -19,6 +23,10 @@ public class AuthService {
     private final String CLIENT_ID = "900575659421-q7u2890lr94ik4o440mqmi1stj7sm6ik.apps.googleusercontent.com"; // 구글 클라이언트 ID
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider; // JwtTokenProvider 추가
+    @Value("${jwt.secret}")
+    private String secret;
+    @Autowired
+    private JwtTokenValidator jwtTokenValidator; // JwtTokenValidator 주입
 
     @Autowired
     public AuthService(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
@@ -74,6 +82,46 @@ public class AuthService {
         } catch (GeneralSecurityException | IOException e) {
             System.out.println("Token verification failed: " + e.getMessage());
             return null;
+        }
+    }
+    public boolean updateUserProfile(String token, UserDto userDto) {
+        //String ageRangeString = userDto.getAgeRange();
+        //String genderString = userDto.getGender();
+        //int ageRange = Integer.parseInt(ageRangeString);
+        //int gender = Integer.parseInt(genderString);
+
+        try {
+            // 토큰에서 이메일 추출
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secret)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String email = claims.get("email", String.class);
+            System.out.println("email : " + email);
+            //String email = jwtTokenProvider.extractEmail(token);
+
+            // UserRepository를 사용하여 해당 이메일의 유저 정보 조회
+            User existingUser = userRepository.findByEmail(email);
+            //System.out.println("existingUser : " + existingUser);
+
+            if (existingUser != null) {
+                // 기존 유저 정보 업데이트
+                //existingUser.setAgeRange(ageRange);
+                //existingUser.setGender(gender);
+                //existingUser.setNickname(userDto.getNickname());
+
+                // UserRepository를 사용하여 업데이트된 유저 정보 저장
+                userRepository.save(existingUser);
+
+                return true; // 업데이트 성공
+            } else {
+                // 해당 이메일로 등록된 유저가 없을 경우 실패
+                return false;
+            }
+        } catch (Exception e) {
+            // 예외 발생 시 실패
+            return false;
         }
     }
 }
