519a77114ca07e2616db5ed0e80ea12f6d76b102
qjvk2880 <qjvk2880@gmail.com>
2023-09-09 17:25:43 +0900

    Refactor : pushAlarm 기능의 이동으로 인한 기존 코드 삭제

--- src/main/java/com/example/hanium2023/service/ArrivalInfoService.java
+++ src/main/java/com/example/hanium2023/service/ArrivalInfoService.java
@@ -30,8 +30,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ArrivalInfoService {
     private final PublicApiService publicApiService;
-    private final UserRepository userRepository;
-    private final RedisUtil redisUtil;
 
     public List<ArrivalInfoStationInfoPageResponse> getArrivalInfo(String stationName) {
         Predicate<ArrivalInfoApiResult> arrivalInfoFilter = this::removeExpiredArrivalInfo;
@@ -55,20 +53,6 @@ public class ArrivalInfoService {
                 .collect(Collectors.toList());
     }
 
-    public List<ArrivalInfoPushAlarm> getRealTimeInfoForPushAlarm(String stationName, String exitName) {
-        Predicate<ArrivalInfoApiResult> removeTooFarArrivalInfoFilter = this::removeTooFarArrivalInfo;
-        Predicate<ArrivalInfoApiResult> arrivalInfoFilter = removeTooFarArrivalInfoFilter.and(this::removeInvalidArrivalInfo);
-        List<ArrivalInfoApiResult> arrivalInfoApiResultList = getArrivalInfoFromPublicApi(stationName, arrivalInfoFilter);
-
-        UserDto userDto = new UserDto(userRepository.findById(41L).get());
-
-        return arrivalInfoApiResultList
-                .stream()
-                .map(ArrivalInfoPushAlarm::new)
-                .map(apiResult -> calculateMovingTime(apiResult, stationName, exitName, userDto))
-                .collect(Collectors.toList());
-    }
-
     private List<ArrivalInfoApiResult> getArrivalInfoFromPublicApi(String stationName, Predicate<ArrivalInfoApiResult> filterPredicate) {
         JSONObject apiResultJsonObject = publicApiService.getApiResult(publicApiService.buildRealTimeApiUrl(stationName));
         Optional<JSONArray> jsonArray = Optional.ofNullable((JSONArray) apiResultJsonObject.get("realtimeArrivalList"));
@@ -85,51 +69,6 @@ public class ArrivalInfoService {
         return arrivalInfoApiResultList;
     }
 
-    private ArrivalInfoPushAlarm calculateMovingTime(ArrivalInfoPushAlarm arrivalInfoPushAlarm, String stationName, String exitName, UserDto userDto) {
-        Integer stationId = redisUtil.getStationIdByStationNameAndLineId(stationName, Integer.valueOf(arrivalInfoPushAlarm.getLineId()));
-        double distance = redisUtil.getDistanceByStationIdAndExitName(stationId, exitName);
-        double minMovingSpeed = distance / (double) arrivalInfoPushAlarm.getArrivalTime();
-
-        MovingSpeedInfo movingSpeedInfo = getMovingSpeedInfo(userDto, minMovingSpeed);
-
-        long movingTime = (long) (distance / movingSpeedInfo.getMovingSpeed());
-
-        if (movingSpeedInfo.getMovingSpeed() == -1.0)
-            arrivalInfoPushAlarm.setMessage(movingSpeedInfo.getMovingMessageEnum().getMessage());
-        else
-            arrivalInfoPushAlarm.setMessage(movingTime + "초 동안 " + movingSpeedInfo.getMovingMessageEnum().getMessage());
-
-        arrivalInfoPushAlarm.setMovingTime(movingTime > 0 ? movingTime : 0);
-        arrivalInfoPushAlarm.setMovingSpeedStep(movingSpeedInfo.getMovingMessageEnum().getMovingSpeedStep());
-        arrivalInfoPushAlarm.setMovingSpeed(movingSpeedInfo.getMovingSpeed());
-
-        return arrivalInfoPushAlarm;
-    }
-
-    private MovingSpeedInfo getMovingSpeedInfo(UserDto userDto, double minMovingSpeed) {
-        MovingMessageEnum[] movingMessageEnums = MovingMessageEnum.getMovingMessageEnums();
-        double[] speedBoundary = getSpeedBoundary(userDto);
-
-        for (int i = 0; i < speedBoundary.length; i++) {
-            if (minMovingSpeed <= speedBoundary[i]) {
-                return new MovingSpeedInfo(movingMessageEnums[i], speedBoundary[i]);
-            }
-        }
-
-        return new MovingSpeedInfo(MovingMessageEnum.CANNOT_BOARD, -1.0);
-    }
-
-    private double[] getSpeedBoundary(UserDto userDto) {
-        return new double[]{
-                userDto.getWalkingSpeed(),
-                userDto.getWalkingSpeed() * 1.2,
-                userDto.getWalkingSpeed() * 1.5,
-                userDto.getRunningSpeed() * 0.5,
-                userDto.getRunningSpeed(),
-                userDto.getRunningSpeed() * 1.2
-        };
-    }
-
     private ArrivalInfoApiResult correctArrivalTime(ArrivalInfoApiResult apiResult) {
         LocalDateTime currentTime = TimeUtil.getCurrentTime();
         LocalDateTime targetTime = TimeUtil.getTimeFromString(apiResult.getCreatedAt());
@@ -142,14 +81,6 @@ public class ArrivalInfoService {
 
     private boolean filterArrivalInfoByLineId(ArrivalInfoApiResult arrivalInfo, String lineId) {
         return arrivalInfo.getLineId().equals(lineId);
-    }
-
-    private boolean removeTooFarArrivalInfo(ArrivalInfoApiResult arrivalInfo) {
-        return arrivalInfo.getArrivalCode() != TrainStatusCodeEnum.NOT_CLOSE_STATION.getCode();
-    }
-
-    private boolean removeInvalidArrivalInfo(ArrivalInfoApiResult arrivalInfo) {
-        return arrivalInfo.getArrivalTime() >= 30 && arrivalInfo.getArrivalTime() <= 300;
     }
 
     private boolean removeExpiredArrivalInfo(ArrivalInfoApiResult arrivalInfo) {
